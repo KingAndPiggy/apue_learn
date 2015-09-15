5e20ef0e685af33dac5329d13fcdad2057388931
king <hui497047658@163.com>
2015-09-15 11:52:24 +0800

    aaaaa

--- include/apue1.h
+++ include/apue1.h
@@ -20,19 +20,19 @@
 #include <signal.h> /* for SIG_ERR */
 #define MAXLINE 4096  /* max line length */
 /*
-    * Default file access permissions for new files.
-	*/
+ * Default file access permissions for new files.
+ */
 #define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 /*
-    * Default permissions for new directories.
-	*/
+ * Default permissions for new directories.
+ */
 #define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 typedef void Sigfunc(int); /* for signal handlers */
 #define min(a,b) ((a) < (b) ? (a):(b))
 #define max(a,b) ((a) > (b) ? (a):(b))
 /*
-    * Prototypes for our own functions.
-	*/
+ * Prototypes for our own functions.
+ */
 char *path_alloc(size_t *);
 long open_max(void);
 int set_cloexec(int);
@@ -94,249 +94,249 @@ void WAIT_CHILD(void);
 #include <stdarg.h> /* ISO C variable arguments */
 static void err_doit(int, int, const char *, va_list);
 /*
-    * Nonfatal error related to a system call.
-	 * Print a message and return.
-	 */
-void
+ * Nonfatal error related to a system call.
+ * Print a message and return.
+ */
+	void
 err_ret(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(1, errno, fmt, ap);
-		    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
 }
 /*
-    * Fatal error related to a system call.
-	 * Print a message and terminate.
-	 */
-void 
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
+	void 
 err_sys(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(1, errno, fmt, ap);
-		    va_end(ap);
-			  exit(1);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
 }
 /*
-    * Nonfatal error unrelated to a system call.
-	 * Error code passed as explict parameter.
-	  * Print a message and return.
-	  */
-void
+ * Nonfatal error unrelated to a system call.
+ * Error code passed as explict parameter.
+ * Print a message and return.
+ */
+	void
 err_cont(int error, const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(1, errno, fmt, ap);
-		    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
 }
 /*
-    * Fatal error unrelated to a system call.
-	 * Error code passed as explict parameter.
-	  * Print a message and terminate.
-	  */
-void 
+ * Fatal error unrelated to a system call.
+ * Error code passed as explict parameter.
+ * Print a message and terminate.
+ */
+	void 
 err_exit(int error, const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(1, errno, fmt, ap);
-		    va_end(ap);
-			  exit(1);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	exit(1);
 }
 /*
-    * Fatal error related to a system call.
-	 * Print a message, dump core, and terminate.
-	 */
-void
+ * Fatal error related to a system call.
+ * Print a message, dump core, and terminate.
+ */
+	void
 err_dump(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(1, errno, fmt, ap);
-		    va_end(ap);
-			  abort(); /* core dump and terminate */
-			    exit(1); /* should't get here */
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+	abort(); /* core dump and terminate */
+	exit(1); /* should't get here */
 }
 /*
-    * Nonfatal error unrelated to a system call.
-	 * Print a message and return.
-	 */
-void 
+ * Nonfatal error unrelated to a system call.
+ * Print a message and return.
+ */
+	void 
 err_msg(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(0, 0, fmt, ap);
-		    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
 }
 /*
-    * Fatal error unrelated to a system call.
-	 * Print a message and terminate.
-	 */
-void
+ * Fatal error unrelated to a system call.
+ * Print a message and terminate.
+ */
+	void
 err_quit(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  err_doit(0, 0, fmt, ap);
-		    va_end(ap);
-			  exit(1);
+	va_list ap;
+	va_start(ap, fmt);
+	err_doit(0, 0, fmt, ap);
+	va_end(ap);
+	exit(1);
 }
 /*
-    * Print a message and return to caller.
-	 * Caller specifies "errnoflag"
-	 */
-static void 
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag"
+ */
+	static void 
 err_doit(int errnoflag, int error, const char *fmt, va_list ap)
 {
-	  char buf[MAXLINE];
-	    vsnprintf(buf, MAXLINE-1, fmt, ap);
-		  if(errnoflag){
-			      snprintf(buf+strlen(buf), MAXLINE-strlen(buf) - 1, ": %s", strerror(error));
-				    }
-		    strcat(buf, "\n");
-			  fflush(stdout); /* in case stdout and stderr are the same */
-			    fputs(buf, stderr);
-				  fflush(NULL); /* flushes all stdio output streams */
+	char buf[MAXLINE];
+	vsnprintf(buf, MAXLINE-1, fmt, ap);
+	if(errnoflag){
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf) - 1, ": %s", strerror(error));
+	}
+	strcat(buf, "\n");
+	fflush(stdout); /* in case stdout and stderr are the same */
+	fputs(buf, stderr);
+	fflush(NULL); /* flushes all stdio output streams */
 }
 #include <syslog.h>
 static void log_doit(int, int, int, const char *, va_list ap);
 int log_to_stderr = 0;
 /*
-    * Initialize syslog(), if running as daemon.
-	*/
-void 
+ * Initialize syslog(), if running as daemon.
+ */
+	void 
 log_open(const char *ident, int option, int facility)
 {
-	  if(log_to_stderr == 0){
-		      openlog(ident, option, facility);
-			    }
+	if(log_to_stderr == 0){
+		openlog(ident, option, facility);
+	}
 }
 /*
-    * Nonfatal error related to a system call.
-	 * Print a message with the system's errno value and return.
-	 */
-void 
+ * Nonfatal error related to a system call.
+ * Print a message with the system's errno value and return.
+ */
+	void 
 log_ret(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  log_doit(1, errno, LOG_ERR, fmt, ap);
-		    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
 }
 /*
-    * Fatal error related to a system call.
-	 * Print a message and terminate.
-	 */
-void 
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
+	void 
 log_sys(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  log_doit(1, errno, LOG_ERR, fmt, ap);
-		    va_end(ap);
-			  exit(2);
+	va_list ap;
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
 }
 /*
-    * Nonfatal error unrelated to a system call.
-	 * Print a message and return.
-	 */
-void
+ * Nonfatal error unrelated to a system call.
+ * Print a message and return.
+ */
+	void
 log_msg(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  log_doit(0, 0, LOG_ERR, fmt, ap);
-		    va_end(ap);
+	va_list ap;
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
 }
 /*
-    * Fatal error related to a system call.
-	 * Print a message and terminate.
-	 */
-void 
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
+	void 
 log_quit(const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  log_doit(0, 0, LOG_ERR, fmt, ap);
-		    va_end(ap);
-			  exit(2);
+	va_list ap;
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
 }
 /*
-    * Fatal error related to a system call.
-	 * Error number passed as an explicit parameter.
-	  * Print a message and terminate.
-	  */
-void
+ * Fatal error related to a system call.
+ * Error number passed as an explicit parameter.
+ * Print a message and terminate.
+ */
+	void
 log_exit(int error, const char *fmt, ...)
 {
-	  va_list ap;
-	    va_start(ap, fmt);
-		  log_doit(1, error, LOG_ERR, fmt, ap);
-		    va_end(ap);
-			  exit(2);
+	va_list ap;
+	va_start(ap, fmt);
+	log_doit(1, error, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
 }
 /*
-    * Print a message and return to caller.
-	 * Caller specifies "errnoflag" and "priority".
-	 */
-static void 
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag" and "priority".
+ */
+	static void 
 log_doit(int errnoflag, int error, int priority, const char *fmt, va_list ap)
 {
-	  char buf[MAXLINE];
-	    vsnprintf(buf, MAXLINE-1, fmt, ap);
-		  if(errnoflag){
-			      snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s", strerror(error));
-				    }
-		    strcat(buf, "\n");
-			  if(log_to_stderr){
-				      fflush(stdout);
-					      fputs(buf, stderr);
-						      fflush(stderr);
-							    }else{
-									    syslog(priority, "%s", buf);
-										  }
+	char buf[MAXLINE];
+	vsnprintf(buf, MAXLINE-1, fmt, ap);
+	if(errnoflag){
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s", strerror(error));
+	}
+	strcat(buf, "\n");
+	if(log_to_stderr){
+		fflush(stdout);
+		fputs(buf, stderr);
+		fflush(stderr);
+	}else{
+		syslog(priority, "%s", buf);
+	}
 }
 #include <sys/wait.h>
-void 
+	void 
 pr_exit(int status)
 {
-	  if(WIFEXITED(status))
-		      printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
-	    else if(WIFSIGNALED(status))
-			    printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status), 
+	if(WIFEXITED(status))
+		printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
+	else if(WIFSIGNALED(status))
+		printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status), 
 #ifdef WCOREDUMP
-						    WCOREDUMP(status)?"(core file generated)":"");
+				WCOREDUMP(status)?"(core file generated)":"");
 #else
-		    "");
+	"");
 #endif
-			 else if(WIFSTOPPED(status))
-				    printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+	else if(WIFSTOPPED(status))
+		printf("child stopped, signal number = %d\n", WSTOPSIG(status));
 }
- 
-void
+
+	void
 pr_mask(const char *str)
 {
-	  sigset_t sigset;
-	    int errno_save;
-		  errno_save = errno;
-		    if(sigprocmask(0, NULL, &sigset) < 0){
-				    err_ret("sigprocmask error");
-					  }else {
-						      printf("%s", str);
-							       
-							      if(sigismember(&sigset, SIGINT))
-									        printf(" SIGINT");
-								      if(sigismember(&sigset, SIGQUIT))
-										        printf(" SIGQUIT");
-									      if(sigismember(&sigset, SIGUSR1))
-											        printf(" SIGUSR1");
-										      if(sigismember(&sigset, SIGALRM))
-												        printf(" SIGALRM");
-											      printf("\n");
-												    }
-			  errno = errno_save;
+	sigset_t sigset;
+	int errno_save;
+	errno_save = errno;
+	if(sigprocmask(0, NULL, &sigset) < 0){
+		err_ret("sigprocmask error");
+	}else {
+		printf("%s", str);
+
+		if(sigismember(&sigset, SIGINT))
+			printf(" SIGINT");
+		if(sigismember(&sigset, SIGQUIT))
+			printf(" SIGQUIT");
+		if(sigismember(&sigset, SIGUSR1))
+			printf(" SIGUSR1");
+		if(sigismember(&sigset, SIGALRM))
+			printf(" SIGALRM");
+		printf("\n");
+	}
+	errno = errno_save;
 }
 #endif
